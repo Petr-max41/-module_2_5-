d5f80ca513b5555a8854e6e8df90492eb3a592ff
Petr-max41 <krilof25@mail.ru>
2024-10-04 18:40:16 +0500

    Add files via upload

--- 'module_2_5'.py
+++ 'module_2_5'.py
@@ -1,9 +1,11 @@
 def get_matrix(n, m, value):
     matrix = []
+    if n <= 0 or m <= 0:
+        return []
     for i in range(n):
         matrix.append([])
         for j in range(m):
-          matrix[i].append(value)
+            matrix[i].append(value)
     return matrix
 print(get_matrix(2,2,10))
 print(get_matrix(3,5,42))
